04ce945066f9a3c779b89298009a2f86eb290470
Diana-Camz <dcampos0495@gmail.com>
2025-08-12 13:45:36 -0600

    feat: create sign up screen with styles

---
+++ app/signUp.tsx
@@ -0,0 +1,81 @@
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import CustomText from "@/components/CustomText";
+import { signUp } from "@/styles/screens/sign-up";
+import { colors, spacing } from "@/themes";
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
+import { router } from "expo-router";
+import React from "react";
+import {
+    StyleSheet,
+    TouchableOpacity,
+    View
+} from "react-native";
+
+export default function logIn() {
+  return (
+    <View style={[spacing.safeArea, spacing.safeStatusBar, signUp.container]}>
+       <View>
+         <TouchableOpacity 
+            onPress={() => router.back()}
+            style={signUp.goBackButton}
+         >
+            <Entypo name="chevron-small-left" size={50} color={colors.black} />
+         </TouchableOpacity>
+         <CustomText
+            type="TitleBig"
+            text={"Crear cuenta"}
+            numberOfLines={2}
+         />
+       </View>
+        <View style={signUp.containerInputs}>
+            <CustomInput 
+            type={'text'}
+            label={'Nombre'}
+            placeholder={'Nombre Completo'}
+            />
+            <CustomInput 
+            type={'text'}
+            label={'Email'}
+            placeholder={'Email'}
+            />
+            <CustomInput 
+            type={'text'}
+            label={'Número de celular'}
+            placeholder={'Número de celular'}
+            />
+            <CustomInput 
+            type={'text'}
+            label={'Cumpleaños'}
+            placeholder={'Cumpleaños'}
+            />
+        </View>
+        <View style={signUp.containerConditions}>
+            <View style={signUp.containerCheckBox}>
+                <Feather name={'square'} size={30} color={colors.gray}/>
+            </View>
+           <View style={signUp.containerConditionsText}>
+             <CustomText type="TextSmall">
+                Al crear una cuenta, aceptas nuestros{" "}
+                <CustomText
+                type="linkSmall"
+                onPress={() => console.log("Términos clic")}
+                >
+                Términos y Condiciones
+                </CustomText>
+             </CustomText>
+           </View>
+        </View>
+       <View  style={signUp.containerButton}>
+         <CustomButton title="Crear cuenta" onPress={() => {}} disabled={false}/>
+       </View>
+        <View style={signUp.containerLogin}>
+            <CustomText type={'TextRegular'} text={'¿Ya tienes una cuenta?'} />
+            <TouchableOpacity onPress={() => {}}>
+                <CustomText type={'link'} text={'Inicia sesión'}/>
+            </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
---
+++ styles/screens/sign-up.ts
@@ -0,0 +1,62 @@
+import { colors } from '@/themes';
+import { StyleSheet, ViewStyle } from 'react-native';
+
+export interface CreateBusinessScreenStyles {
+  container: ViewStyle;
+  goBackButton: ViewStyle;
+  containerInputs: ViewStyle;
+  containerConditions: ViewStyle;
+  containerCheckBox: ViewStyle;
+  containerConditionsText: ViewStyle;
+  containerButton: ViewStyle;
+  containerLogin: ViewStyle;
+}
+
+export const signUp = StyleSheet.create<CreateBusinessScreenStyles>({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    paddingBottom: 40,
+  },
+  goBackButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    marginBottom: 25,
+    backgroundColor: colors.sandLight,
+  },
+  containerInputs: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 'auto',
+    height: 'auto',
+    gap: 12,
+  },
+  containerConditions: {
+    width: 380,
+    paddingLeft: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  containerCheckBox: {
+    marginRight: 8,
+  },
+  containerConditionsText: {
+    width: 335,
+   flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  containerButton: {
+    alignItems: 'center',
+  },
+  containerLogin: { 
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingVertical: 16,
+    gap: 5,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.gray,
+    borderTopColor: colors.gray,
+},
+});
