06ff6e20c4ac6fbe23c7cafe15e35e662052f109
beatrizheav <beatriz.heav@gmail.com>
2025-08-18 12:48:03 -0700

    feat: add addressView component to the screen

--- app/(tabs)/order.tsx
+++ app/(tabs)/order.tsx
@@ -1,10 +1,11 @@
-// app/(tabs)/ordena.tsx
-import { Text, View } from "react-native";
+import AddressView from "@/components/AddressView";
+import { spacing } from "@/themes";
+import { View } from "react-native";
 
 export default function Ordena() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Pantalla Ordena</Text>
+    <View style={[spacing.safeArea, spacing.safeStatusBar]}>
+      <AddressView />
     </View>
   );
 }
